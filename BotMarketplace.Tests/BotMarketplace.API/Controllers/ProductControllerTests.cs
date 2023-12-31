92164ff507b678cc2c99f1a38edb9892de39052e
brunomileto <brunomileto@outlook.com>
2023-12-31 12:36:42 -0300

    (tests) add product controller tests

---
+++ BotMarketplace.Tests/BotMarketplace.API/Controllers/ProductControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Bogus;
+using BotMarketplace.API;
+using BotMarketplace.API.Controllers;
+using BotMarketplace.API.DTOs.Products;
+using BotMarketplace.Tests.Factories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BotMarketplace.Tests.BotMarketplace.API.Controllers
+{
+    [TestClass]
+    public class ProductControllerTests : BasicControllerTests<ProductBaseDTO, ProductController>
+    {
+        public override Faker<ProductBaseDTO> CreateFaker()
+        {
+            return ProductFakeFactory.FakeProductBaseDTOMaker();
+        }
+
+    }
+}
