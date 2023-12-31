4c75140a7fce4735f46339b75d208baed672f9e2
brunomileto <brunomileto@outlook.com>
2023-12-31 12:12:19 -0300

    (tests) update user controller tests so it implements basic controller tests

--- BotMarketplace.Tests/BotMarketplace.API/Controllers/UserControllerTests.cs
+++ BotMarketplace.Tests/BotMarketplace.API/Controllers/UserControllerTests.cs
@@ -16,65 +16,69 @@ using Microsoft.AspNetCore.Mvc;
 namespace BotMarketplace.Tests.BotMarketplace.API.Controllers
 {
     [TestClass]
-    public class UserControllerTests
+    public class UserControllerTests : BasicControllerTests<UserBaseDTO, UserController>
     {
-        private readonly HttpClient _client;
-        private Faker<UserBaseDTO> _faker;
-        private UserBaseDTO _user;
-        private readonly GetPaginatedParamsDelegate _getPaginatedParams;
+        public override Faker<UserBaseDTO> CreateFaker ()
+        {
+            return UserFakeFactory.FakeUserBaseDTOMaker();
+        }
+        //private readonly HttpClient _client;
+        //private Faker<UserBaseDTO> _faker;
+        //private UserBaseDTO _user;
+        //private readonly GetPaginatedParamsDelegate _getPaginatedParams;
         
-        public string GetControllerRoute<TController>() where TController : ControllerBase
-        {
-            var controllerName = typeof(TController).Name.Replace("Controller", "");
-            var routeAttribute = typeof(TController).GetCustomAttributes(typeof(RouteAttribute), true)
-                                                    .FirstOrDefault() as RouteAttribute;
+        //public string GetControllerRoute<TController>() where TController : ControllerBase
+        //{
+        //    var controllerName = typeof(TController).Name.Replace("Controller", "");
+        //    var routeAttribute = typeof(TController).GetCustomAttributes(typeof(RouteAttribute), true)
+        //                                            .FirstOrDefault() as RouteAttribute;
 
-            return routeAttribute?.Template.Replace("[controller]", controllerName.ToLowerInvariant()) ?? string.Empty;
-        }
+        //    return routeAttribute?.Template.Replace("[controller]", controllerName.ToLowerInvariant()) ?? string.Empty;
+        //}
 
-        public UserControllerTests()
-        {
-            var factory = new CustomWebApplicationFactory<Program>();
+        //public UserControllerTests()
+        //{
+        //    var factory = new CustomWebApplicationFactory<Program>();
             
-            GetPaginatedParamsDelegate getPaginatedParamsDelegate = new GetPaginatedParamsDelegate(factory.GetPaginatedParams);
-            _getPaginatedParams = getPaginatedParamsDelegate;
+        //    GetPaginatedParamsDelegate getPaginatedParamsDelegate = new GetPaginatedParamsDelegate(factory.GetPaginatedParams);
+        //    _getPaginatedParams = getPaginatedParamsDelegate;
             
-            _client = factory.CreateClient();
+        //    _client = factory.CreateClient();
             
-            _faker = UserFakeFactory.FakeUserBaseDTOMaker();
-            _user = _faker.Generate();
-        }
-        [TestMethod]
-        public async Task CreateUser_ShouldReteurnCreated()
-        {
-            var newUser = _faker.Generate();
+        //    _faker = UserFakeFactory.FakeUserBaseDTOMaker();
+        //    _user = _faker.Generate();
+        //}
+        //[TestMethod]
+        //public async Task CreateUser_ShouldReteurnCreated()
+        //{
+        //    var newUser = _faker.Generate();
 
-            var response = await _client.PostAsJsonAsync(GetControllerRoute<UserController>(), newUser);
+        //    var response = await _client.PostAsJsonAsync(GetControllerRoute<UserController>(), newUser);
 
-            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
-            var createdUser = await response.Content.ReadFromJsonAsync<UserBaseDTO>();
-            Assert.IsNotNull(createdUser);
-            Assert.AreEqual(newUser.Name, createdUser.Name);
-        }
+        //    Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+        //    var createdUser = await response.Content.ReadFromJsonAsync<UserBaseDTO>();
+        //    Assert.IsNotNull(createdUser);
+        //    Assert.AreEqual(newUser.Name, createdUser.Name);
+        //}
 
-        [TestMethod]
-        public async Task GetAllUsers_ShouldReturnOk()
-        {
-            var pageNumber = 1;
-            var perPage = 10;
-            var queryString = _getPaginatedParams(pageNumber, perPage);
-            var testResponse = await _client.PostAsJsonAsync(GetControllerRoute<UserController>(), _user);
-            var userBaseDTO = await testResponse.Content.ReadFromJsonAsync<UserBaseDTO>();
-            //var response = await _client.GetAsync($"/api/user?{queryString}");
-            var response = await _client.GetAsync($"{GetControllerRoute<UserController>()}?{queryString}");
+        //[TestMethod]
+        //public async Task GetAllUsers_ShouldReturnOk()
+        //{
+        //    var pageNumber = 1;
+        //    var perPage = 10;
+        //    var queryString = _getPaginatedParams(pageNumber, perPage);
+        //    var testResponse = await _client.PostAsJsonAsync(GetControllerRoute<UserController>(), _user);
+        //    var userBaseDTO = await testResponse.Content.ReadFromJsonAsync<UserBaseDTO>();
+        //    //var response = await _client.GetAsync($"/api/user?{queryString}");
+        //    var response = await _client.GetAsync($"{GetControllerRoute<UserController>()}?{queryString}");
 
-            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        //    Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
 
-            var usersPaginated = await response.Content.ReadFromJsonAsync<PaginationResponse<UserBaseDTO>>();
-            var user = usersPaginated!.Items.ToList()[0];
-            Assert.IsNotNull(usersPaginated);
-            Assert.IsNotNull(usersPaginated.Items);
-            Assert.IsNotNull(usersPaginated.Items.Where(u => u.Name == userBaseDTO!.Name && u.Email == userBaseDTO!.Email));
-        }
+        //    var usersPaginated = await response.Content.ReadFromJsonAsync<PaginationResponse<UserBaseDTO>>();
+        //    var user = usersPaginated!.Items.ToList()[0];
+        //    Assert.IsNotNull(usersPaginated);
+        //    Assert.IsNotNull(usersPaginated.Items);
+        //    Assert.IsNotNull(usersPaginated.Items.Where(u => u.Name == userBaseDTO!.Name && u.Email == userBaseDTO!.Email));
+        //}
     }
 }
