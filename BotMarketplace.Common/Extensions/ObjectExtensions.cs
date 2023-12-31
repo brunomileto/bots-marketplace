f664b1bfaa416235e89d3b5e259c5ba5db702607
brunomileto <brunomileto@outlook.com>
2023-12-31 12:34:31 -0300

    (feat) create ToJson object extension method

---
+++ BotMarketplace.Common/Extensions/ObjectExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BotMarketplace.Common.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static string ToJson(this object obj, JsonSerializerOptions? options = null)
+        {
+            return JsonSerializer.Serialize(obj, options ?? new JsonSerializerOptions());  
+        }
+    }
+}
