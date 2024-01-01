acc4a735a63e56a483a1957485e0dae0119e53c8
brunomileto <brunomileto@outlook.com>
2024-01-01 18:38:24 -0300

    (feat) create EnumTransactionStatus

---
+++ BotMarketplace.Core/Enums/EnumTransactionStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotMarketplace.Core.Enums
+{
+    public enum EnumTransactionStatus
+    {
+        Success,
+        Created,
+        Deleted,
+        Cancelled_by_seller,
+        Cancelled_by_buyer,
+        Waiting_payment,
+        Waiting_liberation,
+        Cancelled
+    }
+}
